a975dddc13b9051a50fdfd28e979d768bfadcc70
lucio-crDEV <lu@LAPTOP-3D7MI5EC.localdomain>
2022-07-02 16:09:14 -0400

    form para agregar heroe estructurado

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,14 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { Heroe, Publisher } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html'
 })
 export class AgregarComponent implements OnInit {
+  
+  heroe: Heroe = {
+    superhero: '',
+    alter_ego: '',
+    characters: '',
+    first_appearance: '',
+    publisher: Publisher.DCComics,
+    alt_img: ''
+  }
 
-  constructor() { }
+  publishers = [
+    {
+      id: 'DC Comics',
+      desc: 'DC - Comics'
+    },
+    {
+      id: 'Marvel Comics',
+      desc: 'Marvel - Comics'
+    },
+  ]
+
+  constructor(
+    private heroesServices: HeroesService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+
+  guardar(){
+
+    if( this.heroe.superhero.trim().length === 0 && this.heroe.alt_img ==='' ){ return }
+
+    this.heroesServices.agregarHeroe( this.heroe )
+      .subscribe( res => {
+        console.log('Respuesta', res);
+        
+      } )
+    
   }
 
 }
